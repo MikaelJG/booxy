c835b5abeb89051b2738879c6d6099a2c555806f
MikaelJG <mjgonsalves@protonmail.com>
2023-05-09 19:04:42 -0400

    struct

---
+++ booxy.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+struct Chapter {
+    // std::string e;
+    int num;
+    // print is true by default
+    // bool isPrintable;
+};
+
+int main() {
+
+    // for all in this dir
+    // create a chapter object 
+
+    // Create a chapter object
+    Chapter shell;
+
+    // Set the object's properties
+    // john.num = "John";
+    john.num = 22;
+    // john.isMale = true;
+
+    // Print the object's properties
+    cout << "Name: " << john.name << endl;
+    cout << "Age: " << john.age << endl;
+    cout << "Is Male: " << (john.isMale ? "Yes" : "No") << endl;
+
+    return 0;
