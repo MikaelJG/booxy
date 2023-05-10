5257e2b6a91eccd80de4643d2c3fe485deb41499
MikaelJG <mjgonsalves@protonmail.com>
2023-05-10 08:56:44 -0400

    slow progress argv

--- booxy.cpp
+++ booxy.cpp
@@ -22,15 +22,37 @@ std::vector<std::string> GetDocs(Chapter ch) {
     return ch.TexDocs;
 }
 
+std::string getParentDirectory(const std::string& path) {
+    size_t found = path.find_last_of("/\\");
+    if (found != std::string::npos) {
+        return path.substr(0, found);
+    }
+    return "";
+}
 // sort chapters.num 
 // to print in the right order
 
 int main(int argc, char* argv[]) {
-    std::cout << "Number of arguments: " << argc << '\n';
-    std::cout << "Arguments:" << std::endl;
-    for (int i = 0; i < argc; i++) {
-        std::cout << " " << argv[i] << '\n';
-    }
+
+    std::string programPath = argv[0];
+    std::string parentDirectory = getParentDirectory(programPath);
+    std::cout << "Parent Directory: " << parentDirectory << '\n';
+    std::cout << parentDirectory << "outside of if";
+
+
+    // std::string chap_one_path = parentDirectory + "/chap_1";
+    // std::string chap_two_path = script_path/chap_2";
+    // std::string chap_three_path = script_path/argv[0];
+    // std::string chap_four_path = argv[0];
+
+    // std::cout << script_path << '\n';
+    // std::cout << chap_one_path << '\n';
+
+    // std::cout << "Number of arguments: " << argc << '\n';
+    // std::cout << "Arguments:" << '\n';
+    // for (int i = 0; i < argc; i++) {
+    //     std::cout << " " << argv[i] << '\n';
+    // }
     // get relative path
     // where this program is.
 
