48fd4bfa2926e4e014a91f01ee8cee05c1283a89
MikaelJG <mjgonsalves@protonmail.com>
2023-05-09 20:26:30 -0400

    ternary

--- booxy.cpp
+++ booxy.cpp
@@ -5,12 +5,11 @@
 struct Chapter {
     int num;
     std::vector<std::string> TexDocs;
-
-    bool isToPrint; // true by default
+    bool isToPrint = true;
 };
 
 // give it std::string directory as well.
-std::vector<std::string> GetDocs(std::vector<std::string> docs) {
+std::vector<std::string> GetDocs(std::vector<std::string> docs, bool print) {
     // look in the dir
     // for found .tex, 
     // append in the chapter's vector
@@ -19,6 +18,7 @@ std::vector<std::string> GetDocs(std::vector<std::string> docs) {
         std::cout << docs[integer] << " ";
     }
     std::cout << std::endl;
+    std::cout << (print ? "true" : "false") << '\n'; 
     return docs;
 }
 
@@ -35,7 +35,7 @@ int main() {
     Chapter shell;
     shell.TexDocs = {"I'm", "in", "GetDocs", "Function"};
 
-    GetDocs(shell.TexDocs);
+    GetDocs(shell.TexDocs, shell.isToPrint);
 
     shell.num = 22;
     std::cout << "Chapter number: " << shell.num << '\n';
