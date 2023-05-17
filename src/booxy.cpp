da8571f91c592cb036016882e6bfdc1ffb22f896
MikaelJG <mjgonsalves@protonmail.com>
2023-05-17 09:52:45 -0400

    less shell

--- src/booxy.cpp
+++ src/booxy.cpp
@@ -87,3 +87,32 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
+
+// Bash Example 
+
+
+// compile_latex() {
+//     # .log in log ; .pdf in pdf
+//     mv *.aux log ; mv *.log log ; mv *.pdf pdf
+// 
+//     pdf_files=$(ls ${pdf_path}) 
+// 
+//     cd $pdf_path && pdfunite ${pdf_files} workbook_$date.pdf
+// 
+//     mv workbook* ../$complete_path
+// }
+// 
+// # loop over number of chapters
+// for i in {1..5}; do
+//   path="path_$i"
+//   files=$(ls ${!path})
+// 
+//     for filename in ${files}; do
+//         ext="${filename##*.}"
+//         case $ext in
+//         tex) 
+//             filepath=${!path}/$filename
+//             cp $filepath latex;;
+//         esac
+//     done
+// done
